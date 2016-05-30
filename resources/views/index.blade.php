dfcd1295eaed3c658e825bce763dc966166574ff
Lewis White <lewis@lody.be>
2016-05-30 20:53:19 +0100

    shelve the sorting for now

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -44,7 +44,15 @@
 			<a class="disabled">></a>
 			<a class="disabled">>></a>
 		@endif
-		<div>sort by #id descending</div>
+		{{--
+		maybe i'll want to have sorting one day?
+		<div>sort by
+		<label for="by1"><input type="radio" name="by" id="by1" value="id">id</label>
+		<label for="by2"><input type="radio" name="by" id="by2" value="release_date">updated</label>,
+
+		<label for="sort1"><input type="radio" name="sort" id="sort1" value="asc">asc</label>
+		<label for="sort2"><input type="radio" name="sort" id="sort2" value="desc">desc</label></div>
+		--}}
 		</h1>
 	@else
 		<h1>Latest Posts</h1>
